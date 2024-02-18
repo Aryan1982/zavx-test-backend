461dfa69aba90af458e81db0f1dfe561be422ff1
Aryan1982 <ap1982o3@gmail.com>
2024-02-18 10:55:01 +0530

    removed unwanted lines

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ const secretKey = 'thisismysecretkey';
 const clientId = 'thisismyclientid';
 
 const authorizationCodes = {};
-const redirectUri = 'https://pitangui.amazon.com/api/skill/link/M2AAAAAAAAAAAA';
 
 // Login route
 app.post('/login', (req, res) => {
@@ -38,8 +37,6 @@ app.post('/login', (req, res) => {
   const authorizationCode = Math.random().toString(36).substring(7); // Generate random authorization code
   authorizationCodes[authorizationCode] = { client_id, reqRedirectUri };
   if(user){
-    // res.status(200).json({message:"login succesfull"})
-    // res.redirect(`${reqRedirectUri}?code=${authorizationCode}&state=${reqState}`);
     res.status(200).json({redirect:`${reqRedirectUri}?code=${authorizationCode}&state=${reqState}`});
   }
 });
@@ -69,25 +66,20 @@ app.post('/refresh-token', (req, res) => {
 
 
 app.post('/auth/o2/token', (req, res) => {
-  // Extract data from the request body
   const { grant_type, code, client_id, client_secret, redirect_uri } = req.body;
 
-  // Check if required parameters are present
   if (!grant_type || !code || !client_id || !client_secret || !redirect_uri) {
     return res.status(400).json({ message: 'Missing required parameters' });
   }
 
-  // Check if grant_type is 'authorization_code'
   if (grant_type !== 'authorization_code') {
     return res.status(400).json({ message: 'Invalid grant_type' });
   }
 
-  // Check if client_id and client_secret match the expected values
   if (client_id !== clientId || client_secret !== secretKey) {
     return res.status(401).json({ message: 'Invalid client credentials' });
   }
 
-  // If all checks pass, generate and return an access token
   const accessToken = jwt.sign({ clientId: client_id }, secretKey, { expiresIn: '1h' });
   res.json({ access_token: accessToken });
 });
