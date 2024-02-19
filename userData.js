918887cc8dfede19a71b9d2b3d79ab350cc441c2
Aryan1982 <ap1982o3@gmail.com>
2024-02-19 16:11:14 +0530

    added userappliances

---
+++ userData.js
@@ -0,0 +1,70 @@
+// usersData.js
+
+const userData = [
+        {
+            "id": 1,
+            "username": "user1",
+            "password": "password1",
+            "devices": [
+                {
+                    "applianceId": "endpoint-001-56",
+                    "manufacturerName": "AnnantaLabs",
+                    "modelName": "Smart Switch",
+                    "friendlyName": "Switch Zavx",
+                    "friendlyDescription": "Switch that can only be turned on/off",
+                    "actions": [
+                        "turnOn",
+                        "turnOff"
+                    ],
+                    "additionalApplianceDetails": {}
+                },
+                {
+                    "applianceId": "endpoint-002-56",
+                    "manufacturerName": "AnnantaLabs",
+                    "modelName": "Smart Light",
+                    "friendlyName": "Light Zavx",
+                    "friendlyDescription": "002 Light that is dimmable and can change color and color temperature",
+                    "actions": [
+                        "turnOn",
+                        "turnOff",
+                        "setPercentage",
+                        "incrementPercentage",
+                        "decrementPercentage",
+                        "setColor",
+                        "setColorTemperature",
+                        "incrementColorTemperature",
+                        "decrementColorTemperature"
+                    ],
+                    "additionalApplianceDetails": {}
+                }
+            ]
+        },
+        {
+            "id": 2,
+            "username": "user2",
+            "password": "password2",
+            "devices": [
+                {
+                    "applianceId": "endpoint-003",
+                    "manufacturerName": "Sample Manufacturer",
+                    "modelName": "Smart White Light",
+                    "friendlyName": "White Light Zavx",
+                    "friendlyDescription": "003 Light that is dimmable and can change color temperature only",
+                    "actions": [
+                        "turnOn",
+                        "turnOff",
+                        "setPercentage",
+                        "incrementPercentage",
+                        "decrementPercentage",
+                        "setColorTemperature",
+                        "incrementColorTemperature",
+                        "decrementColorTemperature"
+                    ],
+                    "additionalApplianceDetails": {}
+                }
+            ]
+        }
+    ];
+    
+module.exports = userData;
+    
