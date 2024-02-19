c5ab00da277cb757b7a8a610b5ffab0aea27beb9
Aryan1982 <ap1982o3@gmail.com>
2024-02-19 16:39:54 +0530

    added userdata in index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const jwt = require('jsonwebtoken'); // Import jsonwebtoken
-const userData = require('./usersData');
 
 const app = express();
 const port = 8000;
@@ -21,6 +20,71 @@ const clientId = 'thisismyclientid';
 
 const authorizationCodes = {};
 
+const userData = [
+  {
+      "id": 1,
+      "username": "user1",
+      "password": "password1",
+      "devices": [
+          {
+              "applianceId": "endpoint-001-56",
+              "manufacturerName": "AnnantaLabs",
+              "modelName": "Smart Switch user1",
+              "friendlyName": "Switch Zavx user1",
+              "friendlyDescription": "Switch that can only be turned on/off",
+              "actions": [
+                  "turnOn",
+                  "turnOff"
+              ],
+              "additionalApplianceDetails": {}
+          },
+          {
+              "applianceId": "endpoint-002-56",
+              "manufacturerName": "AnnantaLabs",
+              "modelName": "Smart Light user1",
+              "friendlyName": "Light Zavx user1",
+              "friendlyDescription": "002 Light that is dimmable and can change color and color temperature",
+              "actions": [
+                  "turnOn",
+                  "turnOff",
+                  "setPercentage",
+                  "incrementPercentage",
+                  "decrementPercentage",
+                  "setColor",
+                  "setColorTemperature",
+                  "incrementColorTemperature",
+                  "decrementColorTemperature"
+              ],
+              "additionalApplianceDetails": {}
+          }
+      ]
+  },
+  {
+      "id": 2,
+      "username": "user2",
+      "password": "password2",
+      "devices": [
+          {
+              "applianceId": "endpoint-003",
+              "manufacturerName": "Sample Manufacturer",
+              "modelName": "Smart White Light user2",
+              "friendlyName": "White Light Zavx user2",
+              "friendlyDescription": "003 Light that is dimmable and can change color temperature only",
+              "actions": [
+                  "turnOn",
+                  "turnOff",
+                  "setPercentage",
+                  "incrementPercentage",
+                  "decrementPercentage",
+                  "setColorTemperature",
+                  "incrementColorTemperature",
+                  "decrementColorTemperature"
+              ],
+              "additionalApplianceDetails": {}
+          }
+      ]
+  }
+];
 // Login route
 app.post('/login', (req, res) => {
   // console.log(req.query)
@@ -146,3 +210,4 @@ app.post('/getUserAppliances', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
