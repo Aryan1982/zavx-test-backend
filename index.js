2bbd01dbf17fd37dc54935fb4f26e57bf7723362
Aryan1982 <ap1982o3@gmail.com>
2024-02-19 16:15:42 +0530

    userId => code

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const jwt = require('jsonwebtoken'); // Import jsonwebtoken
+const userData = require('./usersData');
 
 const app = express();
 const port = 8000;
@@ -116,63 +117,31 @@ app.post('/test', (req,res)=>{
   res.json({"message":"success"});
 })
 
+app.post('/getUserAppliances', (req, res) => {
+  console.log('called user appliances');
+  const { token } = req.body;
 
-app.post('/getUserAppliances',(req,res)=>{
-  console.log('called user appliances')
-  const SAMPLE_APPLIANCES = [
-    {
-        "applianceId": "endpoint-001-56",
-        "manufacturerName": "AnnantaLabs",
-        "modelName": "Smart Switch",
-        "friendlyName": "Switch Zavx",
-        "friendlyDescription": "Switch that can only be turned on/off",
-        "actions": [
-            "turnOn",
-            "turnOff"
-        ],
-        "additionalApplianceDetails": {}
-    },
-    {
-        "applianceId": "endpoint-002-56",
-        "manufacturerName": "AnnantaLabs",
-        "modelName": "Smart Light ",
-        "friendlyName": "Light Zavx",
-        "friendlyDescription": "002 Light that is dimmable and can change color and color temperature",
-        "actions": [
-            "turnOn",
-            "turnOff",
-            "setPercentage",
-            "incrementPercentage",
-            "decrementPercentage",
-            "setColor",
-            "setColorTemperature",
-            "incrementColorTemperature",
-            "decrementColorTemperature"
-        ],
-        "additionalApplianceDetails": {}
-    },
-    {
-        "applianceId": "endpoint-003",
-        "manufacturerName": "Sample Manufacturer",
-        "modelName": "Smart White Light",
-        "friendlyName": "White Light Zavx",
-        "friendlyDescription": "003 Light that is dimmable and can change color temperature only",
-        "actions": [
-            "turnOn",
-            "turnOff",
-            "setPercentage",
-            "incrementPercentage",
-            "decrementPercentage",
-            "setColorTemperature",
-            "incrementColorTemperature",
-            "decrementColorTemperature"
-        ],
-        "additionalApplianceDetails": {}
+  console.log(token);
+  jwt.verify(token, secretKey, (err, decoded) => {
+    if (err) {
+      console.error('Error decoding token:', err);
+      return res.status(400).json({ error: 'Invalid token' });
     }
-];
+    console.log('Decoded token:', decoded);
 
-    res.json({"SAMPLE_APPLIANCES":SAMPLE_APPLIANCES})
-})
+    // Find user's devices based on the decoded user ID
+    const userId = decoded.code;
+    const userDevices = userData.find(user => user.id === userId)?.devices;
+
+    if (!userDevices) {
+      return res.status(404).json({ error: 'User devices not found' });
+    }
+
+    // Send the user's devices as a response
+    res.json({ userDevices });
+  });
+});
+
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
